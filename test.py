a68c1a5e03455f9e45eb8584dde5a079459bab14
Vatsal Jain <vatsalpjain@gmail.com>
2025-03-02 21:26:21 +0530

    First commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import cv2
+#C:\Users\vatsa\Downloads\OneDrive\Desktop\openCV\test.py
+# Load the image
+image = cv2.imread('original_image1.jpeg')
+
+# Apply Gaussian Blur
+blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
+
+# Display the original and blurred images
+cv2.imshow("Original", image)
+cv2.imshow("Gaussian Blur", blurred_image)
+
+# Save the blurred image
+cv2.imwrite('blurred_image.jpg', blurred_image)
+
+# Wait for a key press and close all windows
+cv2.waitKey(0)
+cv2.destroyAllWindows()
